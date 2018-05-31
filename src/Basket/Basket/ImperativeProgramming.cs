51a467a0936eec25eaf676a1fa075b44a1d24909
Bazely <nicolas.bazely@gmail.com>
2018-05-31 12:17:23 +0200

    Commit projet

---
+++ src/Basket/Basket/ImperativeProgramming.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Basket;
+using Newtonsoft.Json;
+
+namespace BasketTest
+{
+    public class ImperativeProgramming
+    {
+        private static int CalculBasketAmount(List<BasketLineArticle> basketTestBasketLineArticles)
+        {
+            var amountTotal = 0;
+            foreach (var basketLineArticle in basketTestBasketLineArticles)
+            {
+                // Retrive article from database
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                var assemblyDirectory = Path.GetDirectoryName(path);
+                var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
+                IList<ArticleDatabase> articleDatabases =
+                    JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+#if DEBUG
+                var article = GetArticleDatabaseMock(basketLineArticle.Id);
+#else   
+                var article = GetArticleDatabase(basketLineArticle.Id); 
+#endif 
+              
+                // Calculate amount
+                var amount = 0;
+                switch (article.Category)
+                {
+                    case "food":
+                        amount += article.Price * 100 + article.Price * 12;
+                        break;
+                    case "electronic":
+                        amount += article.Price * 100 + article.Price * 20 + 4;
+                        break;
+                    case "desktop":
+                        amount += article.Price * 100 + article.Price * 20;
+                        break;
+                }
+
+                amountTotal += amount * basketLineArticle.Number;
+            }
+
+            return amountTotal;
+        }
+        public static ArticleDatabase GetArticleDatabaseMock(string id) {
+            switch (id)
+            {
+                case "1": return new ArticleDatabase { Id = "1", Price = 1, Stock = 35, Label = "Banana", Category = "food" };
+                case "2": return new ArticleDatabase { Id = "2", Price = 500, Stock = 20, Label = "Fridge electrolux", Category = "electronic" };
+                case "3": return new ArticleDatabase { Id = "3", Price = 49, Stock = 68, Label = "Chair", Category = "desktop" };
+                default: throw new NotImplementedException();
+            } }
+    }
+}
